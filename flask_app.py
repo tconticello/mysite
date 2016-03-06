0a14d091453cb7556515d511c448b907ad99e784
tomas <conticello@gmail.com>
2016-03-06 05:08:08 +0000

    Configurar base datos

--- flask_app.py
+++ flask_app.py
@@ -2,10 +2,25 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask, redirect, render_template, request, url_for
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 app.config["DEBUG"] = True
+
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="tconticello",
+    password="sisupieras21",
+    hostname="tconticello.mysql.pythonanywhere-services.com",
+    databasename="tconticello$comments",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+
+db = SQLAlchemy(app)
+
+
+
 comments = []
 @app.route("/", methods=["GET", "POST"])
 def index():
